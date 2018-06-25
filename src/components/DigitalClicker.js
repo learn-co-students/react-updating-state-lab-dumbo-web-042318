7598b710264cc2c91f3ce035db41b522fb45be4f
teasacura <teasacuraster@gmail.com>
2018-06-25 18:18:20 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  updateCounter = () => {
+    this.setState((prevState) => {
+      return {timesClicked: prevState.timesClicked + 1};
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.updateCounter}>
+        {this.state.timesClicked}
+      </button>
+    );
+  }
+}
+
+export default DigitalClicker;
